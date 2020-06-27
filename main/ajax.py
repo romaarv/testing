17bde47e9ee8ba2e11c5d0ac8a515443a2452c2e
romaarv <romaarv@gmail.com>
2020-06-27 16:32:38 +0300

    Bug fix

--- main/ajax.py
+++ main/ajax.py
@@ -1,10 +1,8 @@
-from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 
 from .models import *
 
 
-@csrf_exempt
 def check_username_exist(request):
     username=request.POST.get("username")
     user_obj=AdvUser.objects.filter(username=username).exists()
