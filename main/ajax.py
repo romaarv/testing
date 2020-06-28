6028bbf9a44f8ece25e1563b267d25cde13df6ef
romaarv <romaarv@gmail.com>
2020-06-28 11:00:16 +0300

    Добавил декоратор @ajax_required

--- main/ajax.py
+++ main/ajax.py
@@ -3,6 +3,17 @@ from django.http import HttpResponse
 from .models import *
 
 
+def ajax_required(f):
+   def wrap(request, *args, **kwargs):
+       if not request.is_ajax():
+           return HttpResponseBadRequest()
+       return f(request, *args, **kwargs)
+   wrap.__doc__=f.__doc__
+   wrap.__name__=f.__name__
+   return wrap
+
+
+@ajax_required
 def check_username_exist(request):
     username=request.POST.get("username")
     user_obj=AdvUser.objects.filter(username=username).exists()
