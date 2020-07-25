5f9b0f9935e3c35b4806c8745a5ea7a0ad59d9e7
romaarv <romaarv@gmail.com>
2020-07-25 19:22:12 +0300

    Настраиваю отправку почты через ukr.net

--- testing/settings.py
+++ testing/settings.py
@@ -140,17 +140,17 @@ EMAIL_USE_SSL = True
 EMAIL_HOST = 'smtp.ukr.net'
 EMAIL_PORT = 465
 EMAIL_HOST_USER = os.environ.get('DJANGO_EMAIL_HOST_USER', 'secret')
-EMAIL_HOST_PASSWORD = os.environ.get('DJANGO_EMAIL_HOST_PASSWORD', 'secret')
+EMAIL_HOST_PASSWORD = os.environ.get('DJANGO_EMAIL_HOST_PASSWORD', 'arv123')
 DEFAULT_FROM_EMAIL = os.environ.get('DJANGO_DEFAULT_FROM_EMAIL', 'secret')
 
-if DEBUG:
-    pass
-    # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-else:
-    SECURE_HSTS_SECONDS = 3600
-    SECURE_SSL_REDIRECT = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
+# if DEBUG:
+#     pass
+#     # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# else:
+SECURE_HSTS_SECONDS = 3600
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
 AUTHENTICATION_BACKENDS = [
     'social_core.backends.facebook.FacebookOAuth2',
